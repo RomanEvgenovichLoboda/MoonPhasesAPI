102dc628a9b6ed8ddc43f3ddfb6c2a96b6edba0b
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-03-12 21:16:06 +0200

    removeTresh

--- src/main/java/Controllers/MoonController.java
+++ src/main/java/Controllers/MoonController.java
@@ -4,30 +4,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @RestController
 public class MoonController {
     @GetMapping("getMoonCycle")
-    public String getMoonCycle(){
-        LocalDateTime now = LocalDateTime.now();
-        if(now.getDayOfMonth()<=6) return "Waxing Crescent";
-        else if(now.getDayOfMonth()==7) return "Full moon";
-        else if(now.getDayOfMonth()>7 && now.getDayOfMonth()<=14) return "Waning moon";
-        else if(now.getDayOfMonth()>15) return "Last quarter";
-        else if(now.getDayOfMonth()>15 && now.getDayOfMonth()<=20) return "Waning month";
-        else if(now.getDayOfMonth()==21) return "New moon";
-        else if(now.getDayOfMonth()>21 && now.getDayOfMonth()<=27) return "Growing month";
-        else if(now.getDayOfMonth()>27 && now.getDayOfMonth()<30) return "First quarter";
-        else if(now.getDayOfMonth()>=30) return "First quarter";
-        else return "Error";
-    }
+    public String getMoonCycle(){ return getMoonCl(LocalDateTime.now());}
+
     @PostMapping("postMoonCycle")
-    public String postMoonCycle(LocalDateTime date){
-        int Day = date.getDayOfMonth();
+    public String postMoonCycle(LocalDateTime date){ return getMoonCl(date);}
+
+    public String getMoonCl(LocalDateTime ldt){
+        int Day = ldt.getDayOfMonth();
         if(Day<=6) return "Waxing Crescent";
         else if(Day==7) return "Full Moon";
         else if(Day>7 && Day<=14) return "Waning Moon";
